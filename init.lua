6511d6e0f5f9020a7d9b0045f7034968e278d7af
snoopy <snoopy@users.noreply.github.com>
2022-12-28 11:15:43 +0000

    add clang-tidy command

--- init.lua
+++ init.lua
@@ -951,6 +951,13 @@ local project_hydra = hydra.create({
 
   { key = 'b', help = 'blame', action = function() util.gitblame() end },
 
+  { key = 't', help = 'clang-tidy', action = function()
+    local rootpath = io.get_project_root(buffer.filename, true)
+    local filepath = buffer.filename
+    local cmd = [[clang-tidy -checks=\*,-fuchsia\*,-modernize\* -p ]]
+    textadept.run.run_project(nil, cmd .. rootpath .. '/build/compile_commands.json ' .. filepath)
+  end, },
+
   { key = 'f1', help = 'ctags: find local', action = dispatch('ctags_local'), },
   { key = 'f2', help = 'ctags: find global', action = dispatch('ctags_global'), },
   { key = 'f8', help = 'ctags: back', action = dispatch('ctags_back'), },
